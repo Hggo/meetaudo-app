250b62e9254fac2b59030e5284318b3231c673fc
Hugo Rodrigues Gomes da Silva <hugo.ifce@gmail.com>
2020-02-22 14:59:42 -0300

    Create first chart

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { DashboardService } from './dashboard.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DashboardService, SensorData } from './dashboard.service';
+import { Chart } from 'chart.js';
+
 
 @Component({
   selector: 'dashboard',
@@ -7,9 +9,40 @@ import { DashboardService } from './dashboard.service';
   styleUrls: ['dashboard.page.scss']
 })
 export class DashboardPage implements OnInit {
+  @ViewChild('barChart', {static: false}) barChart;
+  private data: SensorData[];
+  public bars: any;
   constructor(private dashboardService: DashboardService) {}
   public async ngOnInit() {
-    const data = await this.dashboardService.getdata();
-    console.log(data);
+    this.data = await this.dashboardService.getdata();
+    this.createBarChart();
+  }
+
+  private createBarChart() {
+    // Battery Volts
+    const field = 'Battery Volts';
+    const replace = 'R';
+    const data = this.data.filter(el => el[field] !== null).map(element => element[field]);
+    const labels = this.data.map(element => new Date(element.Date));
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: 'bar',
+      data: {
+        labels,
+        datasets: [{
+          label: 'Voltagem da bateria',
+          data,
+          backgroundColor: 'rgb(38, 194, 129)',
+          borderColor: 'rgb(38, 194, 129)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            type: 'time'
+          }]
+        }
+      }
+    });
   }
 }
--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -8,8 +8,49 @@ export class DashboardService {
         this.baseUrl = '/api';
     }
 
-    public async getdata() {
+    public async getdata(): Promise<SensorData[]> {
         const response = await this.http.get<string>(`${this.baseUrl}/dashboard/data`, {}).toPromise();
         return JSON.parse(response);
     }
+}
+
+export interface SensorData {
+    'Average Power': string;
+    'Average Power Status': string;
+    'Battery Press Compressor': string;
+    'Battery Press HTR %': string;
+    'Battery Press HTR Status': string;
+    'Battery SH': string;
+    'Battery Status': string;
+    'Battery Volts': string;
+    'CCR Sensor 1': string;
+    'CCR Sensor 2': string;
+    'CCR Sensor 3': string;
+    'CCR Sensor 4': string;
+    'Cold Head Sensor 1 Temperature': string;
+    'Cold Head Sensor Status': string;
+    'Date': number;
+    'ERDU Buttons': string;
+    'Field Current': string;
+    'He 0 %': string;
+    'He 100 %': string;
+    'He Alarm %': string;
+    'He EIS': string;
+    'He Level 1': string;
+    'He Self Test': string;
+    'He Status': string;
+    'He Warning %': string;
+    'Ibutton Serial': string;
+    'Mag psiA': number;
+    'Mag psiA Status': string;
+    'QuhHtr Res1': string;
+    'QuhHtr Res2': string;
+    'Shield Sensor 1': string;
+    'Shield Sensor 2': string;
+    'Shield Status': string;
+    'Supervisory Time': string;
+    'SwtHtr Res1': string;
+    'Turret Sensor 1': string;
+    'Turret Sensor 2': string;
+    'Turret Status': string;
 }
