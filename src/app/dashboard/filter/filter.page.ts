7b6e44f969e9d55373a43613667b531166d82211
Hugo Rodrigues Gomes da Silva <hugo.ifce@gmail.com>
2020-02-23 20:09:14 -0300

    Implement filter modal

--- src/app/dashboard/filter/filter.page.ts
+++ src/app/dashboard/filter/filter.page.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { DashboardService } from '../dashboard.service';
 
 @Component({
   selector: 'filter-page',
   templateUrl: 'filter.page.html',
   styleUrls: ['filter.page.scss']
 })
-export class FilterPage {
-  constructor() {
+export class FilterPage implements OnInit {
+  public dateModel: {start: string, end: string};
+  constructor(private modalController: ModalController,
+              private dashboardService: DashboardService) {
+  }
+
+  public ngOnInit() {
+    this.dateModel = {
+      start: new Date().toISOString(),
+      end: new Date().toISOString()
+    };
+  }
+
+  public filter() {
+    this.close();
+  }
+
+  public close() {
+    this.modalController.dismiss();
   }
 }
